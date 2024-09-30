20db654d5c2ab9f531d60527cb199a0ae485d9e5
Thanda Oo <thandaoo.9999@gmail.com>
2024-09-30 23:49:14 +0200

    Build Create Issue API

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import prisma from '@/prisma/client'
+import { z } from 'zod'
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(1000)
+})
+
+export async function POST (request: NextRequest) {
+  const body = await request.json()
+  const validation = createIssueSchema.safeParse(body)
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 })
+
+  const newIssue = prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description
+    }
+  })
+
+  return NextResponse.json(newIssue, { status: 201 })
+}
---
+++ prisma/migrations/20240930202632_change_model_name_to_issue/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Issues` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Issues`;
+
+-- CreateTable
+CREATE TABLE `Issue` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
