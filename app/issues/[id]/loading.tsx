73998b6515382a88db148a13e742069ff40abf98
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-04 16:52:31 +0200

    Add Additional Loading Skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,23 @@
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import { Box, Card, Flex } from '@radix-ui/themes'
+
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
 
 const LoadingIssueDetailsPage = () => {
-  return <div>loading..</div>
+  return (
+    <Box className='max-w-xl'>
+      <Skeleton />
+      <Flex className='space-x-3' my='2'>
+        <Skeleton width='5rem' />
+        <Skeleton width='8rem' />
+      </Flex>
+      <Card className='prose prose-stone mt-4'>
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  )
 }
 
 export default LoadingIssueDetailsPage
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import React from 'react'
 import ReactMarkDown from 'react-markdown'
 import { notFound } from 'next/navigation'
 import prisma from '@/prisma/client'
@@ -14,7 +13,6 @@ const IssueDetailsPage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
   })
-
   if (!issue) notFound() // no need return cuz the return type is 'never'
   return (
     <div className='space-y-4'>
