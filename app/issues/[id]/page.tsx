e29f331872e66eaef4d8d63fc83cd9cd85c2ae1a
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-05 01:17:19 +0200

    Add an Edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import Link from 'next/link'
+import { Pencil2Icon } from '@radix-ui/react-icons'
 import ReactMarkDown from 'react-markdown'
 import { notFound } from 'next/navigation'
 import prisma from '@/prisma/client'
@@ -15,16 +17,24 @@ const IssueDetailsPage = async ({ params }: Props) => {
   })
   if (!issue) notFound() // no need return cuz the return type is 'never'
   return (
-    <div className='space-y-4'>
-      <Heading>{issue.title}</Heading>
-      <Flex gap='4'>
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose prose-stone mt-4'>
-        <ReactMarkDown>{issue.description}</ReactMarkDown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', sm: '2' }} gap='5'>
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap='4'>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose prose-stone mt-4'>
+          <ReactMarkDown>{issue.description}</ReactMarkDown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
