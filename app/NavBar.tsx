a436632bdbf9c2251e8df6e0cac2446ba2bd97c0
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-07 00:04:57 +0200

    Change NavBar Layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Box } from '@radix-ui/themes'
+import { Box, Container, Flex } from '@radix-ui/themes'
+
 import { GiFrogFoot } from 'react-icons/gi'
 import Link from 'next/link'
 import React from 'react'
@@ -17,40 +18,46 @@ const NavBar = () => {
   const { status, data: session } = useSession()
 
   return (
-    <nav className='flex space-x-5 px-6 py-4 items-center border-b'>
-      <Link href='/'>
-        <GiFrogFoot />
-      </Link>
-      <ul className='flex space-x-5'>
-        {links.map(link => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classNames({
-                'text-zinc-700':
-                  link.href.substring(2) ===
-                  currentPath.substring(2, link.href.length),
-
-                'text-zinc-400': link.href !== currentPath,
-                'hover:text-zinc-500 transition-colors': true
-              })}
-              // className={`${
-              //   link.href === currentPath ? 'text-zinc-300' : 'text-zinc-500'
-              // } hover:text-zinc-100 transition-colors`}
-            >
-              {link.label}
+    <nav className='px-6 py-4 border-b'>
+      <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='3'>
+            <Link href='/'>
+              <GiFrogFoot />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === 'authenticated' && (
-          <Link href='/api/auth/signout'>Logout</Link>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href='/api/auth/signin'>Login</Link>
-        )}
-      </Box>
+            <ul className='flex space-x-5'>
+              {links.map(link => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classNames({
+                      'text-zinc-700':
+                        link.href.substring(2) ===
+                        currentPath.substring(2, link.href.length),
+
+                      'text-zinc-400': link.href !== currentPath,
+                      'hover:text-zinc-500 transition-colors': true
+                    })}
+                    // className={`${
+                    //   link.href === currentPath ? 'text-zinc-300' : 'text-zinc-500'
+                    // } hover:text-zinc-100 transition-colors`}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href='/api/auth/signout'>Logout</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='/api/auth/signin'>Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   )
 }
