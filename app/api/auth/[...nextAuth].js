47702513a5b74c3cc17a4c5e7fd0709fbca0f3f4
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-06 16:44:33 +0200

    Add Google Provider

--- app/api/auth/[...nextAuth].js
+++ app/api/auth/[...nextAuth].js
@@ -1,13 +1,14 @@
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth"
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+     GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })
+
     // ...add more providers here
   ],
 }
