509440e2a8cd77c6c336a1963936e78979393925
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-07 22:10:20 +0200

    Refactor AssigneeSelect

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,38 +3,31 @@
 import { Issue, User } from '@prisma/client'
 import toast, { Toaster } from 'react-hot-toast'
 
-import React from 'react'
+import { Fragment } from 'react'
 import { Select } from '@radix-ui/themes'
 import Skeleton from '@/app/components/Skeleton'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000, //cache the data for 60 secs
-    retry: 3 // try 3 more times fetching the data, then set the error
-  })
+  const { data: users, error, isLoading } = useUsers()
 
   if (isLoading) return <Skeleton />
   if (error) return null
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId || null
+      })
+      .catch(() => toast.error("Changes couldn't be saved."))
+  }
+
   return (
-    <>
+    <Fragment>
       <Select.Root
         defaultValue={issue.assignedToUserId || 'none'}
-        onValueChange={userId => {
-          axios
-            .patch('/api/issues/' + issue.id, {
-              assignedToUserId: userId || null
-            })
-            .catch(() => toast.error("Changes couldn't be saved."))
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
@@ -50,8 +43,16 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         </Select.Content>
       </Select.Root>
       <Toaster />
-    </>
+    </Fragment>
   )
 }
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, //cache the data for 60 secs
+    retry: 3 // try 3 more times fetching the data, then set the error
+  })
 
 export default AssigneeSelect
