41e7e30d9f204907e8ce4afa4986eb1cc8e681b9
Thanda Oo <thandaoo.9999@gmail.com>
2024-10-08 13:58:46 +0200

    Build IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,33 @@
 import { Button, Flex, Heading, Section } from '@radix-ui/themes'
 
+import IssueSummary from './IssueSummary'
+import LatestIssues from '@/LatestIssues'
 import Link from 'next/link'
-import Pagination from './components/Pagination'
+import prisma from '@/prisma/client'
 
-export default function Home ({
-  searchParams
-}: {
-  searchParams: { page: string }
-}) {
+export default async function Home () {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' }
+  })
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' }
+  })
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' }
+  })
+
   return (
-    <Section className='space-y-5 flex flex-col items-center  bg-slate-100'>
-      <Heading size='8'>Welcome to Frog!</Heading>
-      <Flex gapX='5'>
-        <Button size='4' variant='surface'>
-          <Link href='/issues/list'>Go to Issues</Link>
-        </Button>
-      </Flex>
-      <Pagination
-        itemsPerPage={20}
-        totalItems={100}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </Section>
+    <Flex direction='column' gap='4'>
+      <Section className='space-y-3 flex flex-col items-center  bg-slate-100'>
+        <Heading size='8'>Welcome to Frog!</Heading>
+        <Flex gapX='5'>
+          <Button size='3' variant='surface'>
+            <Link href='/issues/list'>Go to Issues</Link>
+          </Button>
+        </Flex>
+      </Section>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+    </Flex>
   )
 }
